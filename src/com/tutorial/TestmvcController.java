4faa4e81d15717da1424422097ceb0e3adac3c92
root <root@localhost>
2018-11-07 20:42:30 +0000

    first commit

---
+++ src/com/tutorial/TestmvcController.java
@@ -0,0 +1,21 @@
+package com.tutorial;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/mvc")
+public class TestmvcController {
+    @RequestMapping(method = RequestMethod.GET)public String printAlgo(ModelMap model){
+        model.addAttribute("message","Hi MVC");
+        return "hi mvc";
+        }
+
+
+
+
+}
+
+
---
+++ src/com/tutorial/app.java
@@ -0,0 +1,15 @@
+package com.tutorial;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class app {
+
+
+    public static void main() {
+        new TestmvcController();
+
+    }
+
+
+}
